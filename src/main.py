ee225b5976e21f42c697b70b32c2a9db314bf2b4
Derek Rodger <drodger@protonmail.com>
2022-04-24 15:05:28 -0700

    Added keyboard support for moving spaceman and Mars

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 WIDTH = 1920
 HEIGHT = 1080
 player_x = 600  
-player = 350
+player_y = 350
+mars_x = 900
+mars_y = 150
 
 def draw():
     screen.blit(images.backdrop, (0, 0))
-    screen.blit(images.mars, (900, 150))
-    screen.blit(images.ship, (450, 200))
+    screen.blit(images.mars, (mars_x, mars_y))
+    screen.blit(images.ship, (1000, 200))
+    screen.blit(images.astronaut, (player_x, player_y)) 
+    
+def game_loop():
+    global player_x, player_y
+    global mars_x, mars_y
+    if keyboard.d:
+        player_x+= 10
+    elif keyboard.a:
+        player_x-=10
+    elif keyboard.w:
+        player_y-=10
+    elif keyboard.s:
+        player_y+=10
+    elif keyboard.i:
+        mars_y-=10
+    elif keyboard.j:
+        mars_x -=10
+    elif keyboard.l:
+        mars_x +=10
+    elif keyboard.k:
+        mars_y +=10
+                
+clock.schedule_interval(game_loop, 0.03)
